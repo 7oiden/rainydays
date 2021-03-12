aa986b55723063356e85d6e21a00327f6f7fffb6
7oiden <7oiden@gmail.com>
2021-03-12 12:56:42 +0100

    newsletter form updated

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".newsletter");
+const emailError = document.querySelector("#email-error");
+const success = document.querySelector(".success");
+const button = document.querySelector("#join");
+const email = document.querySelector("#email");
+const label = document.querySelector("#label");
+const container = document.querySelector(".label-container");
+
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (validateEmail(email.value)) {
+    emailError.style.visibility = "hidden";
+  } else {
+    emailError.style.visibility = "visible";
+  }
+  if (validateEmail(email.value)) {
+    label.style.display = "none";
+    container.innerHTML = `<label id="success" for="newsletter">Subscription was successfull!</label>`;
+  }
+}
+
+form.addEventListener("submit", validateForm);
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
